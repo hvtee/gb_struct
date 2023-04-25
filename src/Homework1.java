b339858691ca2c56d1c49b42aaa8ba7c3f7aea46
Artem <milkartem2@mail.ru>
2023-04-25 16:09:02 +0300

    Hw1

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,5 +1,57 @@
+import java.util.Arrays;
+
 public class Homework1 {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        int[] arr = {12, 11, 13, 5, 6, 7};
+        HeapSort heapSort = new HeapSort();
+        heapSort.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    public static class HeapSort {
+        public void sort(int[] arr) {
+            int n = arr.length;
+
+            // Построение кучи (перегруппировка массива)
+            for (int i = n / 2 - 1; i >= 0; i--)
+                heapify(arr, n, i);
+
+            // Один за другим извлекаем элементы из кучи
+            for (int i = n - 1; i >= 0; i--) {
+                // Перемещаем текущий корень в конец
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+
+                // Вызываем процедуру heapify на уменьшенной куче
+                heapify(arr, i, 0);
+            }
+        }
+
+        // Функция для перестройки поддерева с корнем i, чтобы удовлетворить свойству кучи
+        void heapify(int[] arr, int n, int i) {
+            int largest = i; // Инициализируем наибольший элемент как корень
+            int l = 2 * i + 1; // левый = 2*i + 1
+            int r = 2 * i + 2; // правый = 2*i + 2
+
+            // Если левый дочерний элемент больше корня
+            if (l < n && arr[l] > arr[largest])
+                largest = l;
+
+            // Если правый дочерний элемент больше, чем самый большой элемент на данный момент
+            if (r < n && arr[r] > arr[largest])
+                largest = r;
+
+            // Если самый большой элемент не корень
+            if (largest != i) {
+                int swap = arr[i];
+                arr[i] = arr[largest];
+                arr[largest] = swap;
+
+                // Рекурсивно выполняем процедуру heapify для поддерева
+                heapify(arr, n, largest);
+            }
+        }
     }
 }
